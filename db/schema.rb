9b0b2426cc983c38f8b1d8596f2c978eecf21c03
Kodai Hasegawa <chd08720@gmail.com>
2017-04-21 06:35:00 +0000

    kadai add column

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20170421022604) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
 end
